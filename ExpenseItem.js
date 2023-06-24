1fa74aa534de3092447c16acf550e43aed1fdcaf
Kbikash24 <122146950+Kbikash24@users.noreply.github.com>
2023-06-24 22:24:00 +0530

    final page

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,18 +1,17 @@
-import './ExpenseItem.css'
-function ExpenseItem(props){
-    return (<div className='expense-item'>
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <h2>{props.expenseLocation}</h2>
-              
-            <div className='expense-item__price'>{props.amount}</div>
-        </div>
-        
-    </div>
-   
-    );
+const ExpenseItem=(props)=> {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
